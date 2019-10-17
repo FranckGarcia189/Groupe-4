446c764a421d9f8bc3caa7c8058e16c9c186971f
guillaume <guillaume.rachet@gmail.com>
2019-10-17 15:19:10 +0200

    working staline

--- fun.js
+++ fun.js
@@ -1,18 +1,63 @@
 myMove();
 
 function myMove() {
- var elem = document.getElementById("animate");
- var pos = 0;
- var id = setInterval(frame, 2);
- function frame() {
-  if (pos === window.innerWidth) {
-   pos = 0
-  } else {
-   pos++;
-   elem.style.left = pos + "px";
+  var elem = document.getElementById("animate");
+  var pos = 0;
+  var id = setInterval(frame, 2);
+  let toRight = true;
+
+  function frame() {
+    if (toRight) {
+      if (pos === window.innerWidth || pos === window.innerHeight) {
+        pos = window.innerWidth;
+        toRight = !toRight
+      } else {
+        pos++;
+        elem.style.left = pos + "px";
+        elem.style.top = pos + "px";
+      }
+    } else {
+      if (pos === window.innerWidth || pos === window.innerHeight) {
+        pos = 0;
+        toRight = !toRight
+      } else {
+        pos--;
+        elem.style.left = pos + "px";
+        elem.style.top = pos + "px";
+      }
+    }
+
   }
- }
 }
+
+function myMoveStaline() {
+  var elem = document.getElementById("staline");
+  var pos = -300;
+  var id = setInterval(frame, 2);
+
+  function frame() {
+    if (pos === 0) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.bottom = pos + 'px';
+    }
+  }
+
+
+   let inter = setInterval(function () {
+      let staline = document.getElementById("staline");
+      let pos = staline.style.bottom;
+      if (pos < 0) {
+        staline.style.bottom = "0px"
+      } else {
+        clearInterval(inter)
+      }
+    }, 2);
+
+}
+
+
 
 
 //Create the audio tag
@@ -32,13 +77,17 @@ soundFile.play();
 
 //Plays the sound
 function play() {
- //Set the current time for the audio file to the beginning
- soundFile.currentTime = 0.01;
- soundFile.volume = 1;
- let item = document.getElementById("text")
- item.style.color = item.style.color === "red" ?  "black" : "red"
+  //Set the current time for the audio file to the beginning
+  soundFile.currentTime = 0.01;
+  soundFile.volume = 1;
+  let item = document.getElementById("text")
+  item.style.color = item.style.color === "red" ? "black" : "red";
 
- //Due to a bug in Firefox, the audio needs to be played after a delay
- setTimeout(function(){soundFile.play();},1);
+  soundFile.play();
+
+  setTimeout(function () {
+    myMoveStaline();
+  }, 10);
+
 }
 
